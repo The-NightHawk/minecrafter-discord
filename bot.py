2a29a6ec0d14d14f22a66c3e5fa05062089ab485
The-NightHawk <The-NightHawk@users.noreply.github.com>
2021-02-20 17:12:20 +0530

    Added help command

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 from mcuuid.tools import is_valid_minecraft_username
 
-token = 'Your Token Here'
+token = 'NzgxMzQ5ODkwOTQ0MjcwMzY2.X78Wyg.PMPukiusr4CBlnx5VHHTsR8VjkU'
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = '.', intents = intents)
@@ -11,6 +11,15 @@ client = commands.Bot(command_prefix = '.', intents = intents)
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Game('Minecraft Skins'))
+
+client.remove_command('help')
+@client.command()
+async def help(ctx):
+    embed=discord.Embed(title="Help", description="Minecraft Skins is a bot which can show you a specific player's skin based on their username")
+    embed.add_field(name=".skin <username/UUID>", value="Use this command to find the skin of a\n specific player. You can use UUIDs too!", inline=True)
+    embed.add_field(name=".help", value="Shows this command", inline=True)
+    embed.set_footer(text="A bot by TheNightHawk#5516")
+    await ctx.send(embed=embed)
 
 @client.command()
 async def skin(ctx, name):
